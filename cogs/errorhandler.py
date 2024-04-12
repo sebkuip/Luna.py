ee92abf8c8cdfdc1cbc4071937dbef2561012460
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2024-04-12 16:55:48 +0200

    Commented out most of error handler. Errors can log to console according to specifications.

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -15,63 +15,30 @@ class ErrorHandler(commands.Cog):
 
         error = getattr(error, "original", error)
 
-        embed = discord.Embed(
-            title=f"```***AN ERROR OCCURED***```",
-            description=f"Uh oh, something went wrong. The following error appeared:",
-            color=discord.Color.red(),
-        )
-        embed.set_thumbnail(url="https://i.imgur.com/R7ib9Fu.png")
-
-        if isinstance(error, FileNotFoundError):
-            return
-        elif isinstance(error, commands.NotOwner):
-            embed.add_field(
-                name=f"**NotOwner**",
-                value=f"Looks like you are not the owner of the bot and cannot use this command. "
-                f"Developers only >:(",
-            )
-        elif isinstance(error, commands.MemberNotFound):
-            embed.add_field(
-                name=f"**MemberNotFound**",
-                value=f"Looks like you tried to mention a user that doesn't exist. "
-                f"Please try again.",
-            )
-        elif isinstance(error, commands.BadArgument):
-            embed.add_field(
-                name=f"**BadArgument**",
-                value=f"Looks like you entered a wrong argument. Please check carefully. Maybe the "
-                f"channel or user does not exist?\nIf this was an infraction, please manually add "
-                f"the infraction to the database.",
-            )
-        elif isinstance(error, commands.MissingRequiredArgument):
-            # embed.add_field(name=f'**MissingRequiredArgument**',
-            # value=f'Looks like you forgot to enter an argument for this command.')
+        if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(
                 f"Command usage: `>{ctx.command.name} {ctx.command.signature}`"
             )
             return
-        elif isinstance(error, commands.CommandNotFound):
-            embed.add_field(name=f"CommandNotFound", value=f"```{error}```")
-        elif isinstance(error, commands.MissingPermissions):
-            embed.add_field(
-                name=f"**MissingPermissions**",
-                value=f"Looks like you do not have the permission to execute the command.",
-            )
-        elif isinstance(error, commands.BotMissingPermissions):
-            embed.add_field(
-                name=f"**BotMissingPermissions**",
-                value=f"Looks like the bot is missing permissions to execute this command.",
-            )
-        elif isinstance(error, commands.DisabledCommand):
-            embed.add_field(
-                name=f"**DisabledCommand**",
-                value=f"Looks like the command you tried to execute is disabled.",
-            )
-        elif isinstance(error, commands.PrivateMessageOnly):
-            embed.add_field(
-                name=f"**PrivateMessageOnly**",
-                value=f"Looks like the command you tried to execute is only available in DMs.",
-            )
+
+        # elif isinstance(error, FileNotFoundError):
+        #     pass
+        # elif isinstance(error, commands.NotOwner):
+        #     pass
+        # elif isinstance(error, commands.MemberNotFound):
+        #     pass
+        # elif isinstance(error, commands.BadArgument):
+        #     pass
+        # elif isinstance(error, commands.CommandNotFound):
+        #     pass
+        # lif isinstance(error, commands.MissingPermissions):
+        #     pass
+        # elif isinstance(error, commands.BotMissingPermissions):
+        #    pass
+        # elif isinstance(error, commands.DisabledCommand):
+        #     pass
+        # elif isinstance(error, commands.PrivateMessageOnly):
+        #    pass
         # elif isinstance(error, discord.Forbidden):
         #     embed.add_field(
         #         name=f"**Forbidden**",
@@ -81,16 +48,6 @@ class ErrorHandler(commands.Cog):
         # embed.add_field(name=f'**CommandInvokeError**',
         # value=f'Looks like something is wrong with the command. This could be because the page '
         # f'does not exist.')
-        else:
-            embed.add_field(
-                name=f"**UnknownError**",
-                value=f"An unknown error has appeared, check console",
-            )
-            traceback.print_tb(error.__traceback__)
-            print(error)
-
-        await ctx.send(embed=embed)
-
 
 async def setup(bot):
     await bot.add_cog(ErrorHandler(bot))
